43b0fc867934f0aab1441fb8b20cbd036e147615
Konstantin Gründger <kammermark@gmx.de>
2025-04-14 21:34:30 +0200

    use pythonize to create python objects

--- pyo3/src/lib.rs
+++ pyo3/src/lib.rs
@@ -1,23 +1,36 @@
 use aprs_parser::AprsPacket;
 use pyo3::prelude::*;
+use pythonize::pythonize;
 use rayon::prelude::*;
 
-/// Parse an APRS packet from a string.
+/// Parse an APRS packet from a string to a list of JSON strings: List[str]
 #[pyfunction]
-fn parse(packet: &str) -> PyResult<Vec<String>> {
+fn parse_raw(packet: &str) -> PyResult<Vec<String>> {
     let aprs_strings = packet.lines().collect::<Vec<_>>();
-    let dicts = aprs_strings
+    let json_strings = aprs_strings
         .par_iter()
         .map(|aprs_string| {
             serde_json::to_string(&aprs_string.parse::<AprsPacket>().unwrap()).unwrap()
         })
         .collect();
-    Ok(dicts)
+    Ok(json_strings)
+}
+
+/// Parse an APRS packet from a string to a Python object: List[Dict[str, Any]]
+#[pyfunction]
+fn parse(py: Python, packet: &str) -> PyResult<Py<PyAny>> {
+    let aprs_strings = packet.lines().collect::<Vec<_>>();
+    let packets = aprs_strings
+        .par_iter()
+        .map(|aprs_string| aprs_string.parse::<AprsPacket>().unwrap())
+        .collect::<Vec<AprsPacket>>();
+    Ok(pythonize(py, &packets)?.into())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule(name = "aprs_parser")]
 fn aprs_parser_python(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(parse_raw, m)?)?;
     m.add_function(wrap_pyfunction!(parse, m)?)?;
     Ok(())
 }
